827ea525d2e22386a933996f684e0f5121984a67
aherrero <alexhc89@gmail.com>
2018-11-12 18:24:22 +0100

    Shift register with random values for outputs. Improvement of wall watch

--- Software/TMR1_v2_rtcLowDisplay/src/main.cpp
+++ Software/TMR1_v2_rtcLowDisplay/src/main.cpp
@@ -4,8 +4,9 @@
 #include "LowPower.h"
 
 // Constant
-const int BUTTON_PIN_ENTER = 2;    // Button For interruption
+const int BUTTON_PIN_ENTER = 2;
 const int BUTTON_PIN_CH = 3;
+
 const uint8_t  SPRINTF_BUFFER_SIZE = 32;
 const int SERIAL_ACTIVE = 1;
 
@@ -25,57 +26,55 @@ int setMinutes = 0;
 
 static uint8_t secs;
 
-// Function wakeup
-void wakeUp()
-{
-    // Just a handler for the pin interrupt.
-}
-
 // 74HC595 Control
-void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
-  // This shifts 8 bits out MSB first,
-  //on the rising edge of the clock,
-  //clock idles low
+void shiftOut(int myDataPin, int myClockPin, byte myDataOut)
+{
+    // This shifts 8 bits out MSB first,
+    //on the rising edge of the clock,
+    //clock idles low
 
-  //internal function setup
-  int i=0;
-  int pinState;
-  pinMode(myClockPin, OUTPUT);
-  pinMode(myDataPin, OUTPUT);
+    //internal function setup
+    int i=0;
+    int pinState;
+    pinMode(myClockPin, OUTPUT);
+    pinMode(myDataPin, OUTPUT);
 
-  //clear everything out just in case to
-  //prepare shift register for bit shifting
-  digitalWrite(myDataPin, 0);
-  digitalWrite(myClockPin, 0);
-
-  //for each bit in the byte myDataOut�
-  //NOTICE THAT WE ARE COUNTING DOWN in our for loop
-  //This means that %00000001 or "1" will go through such
-  //that it will be pin Q0 that lights.
-  for (i=7; i>=0; i--)  {
+    //clear everything out just in case to
+    //prepare shift register for bit shifting
+    digitalWrite(myDataPin, 0);
     digitalWrite(myClockPin, 0);
 
-    //if the value passed to myDataOut and a bitmask result
-    // true then... so if we are at i=6 and our value is
-    // %11010100 it would the code compares it to %01000000
-    // and proceeds to set pinState to 1.
-    if ( myDataOut & (1<<i) ) {
-      pinState= 1;
-    }
-    else {
-      pinState= 0;
+    //for each bit in the byte myDataOut�
+    //NOTICE THAT WE ARE COUNTING DOWN in our for loop
+    //This means that %00000001 or "1" will go through such
+    //that it will be pin Q0 that lights.
+    for (i=7; i>=0; i--)
+    {
+        digitalWrite(myClockPin, 0);
+
+        //if the value passed to myDataOut and a bitmask result
+        // true then... so if we are at i=6 and our value is
+        // %11010100 it would the code compares it to %01000000
+        // and proceeds to set pinState to 1.
+        if ( myDataOut & (1<<i) )
+        {
+            pinState= 1;
+        }
+        else
+        {
+            pinState= 0;
+        }
+
+        //Sets the pin to HIGH or LOW depending on pinState
+        digitalWrite(myDataPin, pinState);
+        //register shifts bits on upstroke of clock pin
+        digitalWrite(myClockPin, 1);
+        //zero the data pin after shift to prevent bleed through
+        digitalWrite(myDataPin, 0);
     }
 
-    //Sets the pin to HIGH or LOW depending on pinState
-    digitalWrite(myDataPin, pinState);
-    //register shifts bits on upstroke of clock pin
-    digitalWrite(myClockPin, 1);
-    //zero the data pin after shift to prevent bleed through
-    digitalWrite(myDataPin, 0);
-  }
-
-  //stop shifting
-  digitalWrite(myClockPin, 0);
+    //stop shifting
+    digitalWrite(myClockPin, 0);
 }
 
 int displayThrough74HC595(int h1, int h2, int m1, int m2)
@@ -181,72 +180,54 @@ void setup()
 
 void loop()
 {
-
-    // Allow wake up pin to trigger interrupt on low.
-    //attachInterrupt(0, wakeUp, HIGH);
-
-    // Enter power down state with ADC and BOD module disabled.
-    // Wake up when wake up pin is low.
-    //LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-
-    // Disable external pin interrupt on wake up pin.
-    //detachInterrupt(0);
-
-    //// LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
-
-    // Do something here after wake up until next loop to sleep
-
     if(setTimeState < 2)
     {
-      if(digitalRead(BUTTON_PIN_ENTER) == HIGH)
-        setTimeState = setTimeState + 1;
+        if(digitalRead(BUTTON_PIN_ENTER) == HIGH)
+            setTimeState = setTimeState + 1;
 
-      if(digitalRead(BUTTON_PIN_CH) == HIGH)
-      {
-         if(setTimeState == 0)
-         {
-          setHour = setHour + 1;
-         }
-         else if(setTimeState == 1)
-         {
-          setMinutes = setMinutes + 1;
-         }
-      }
+        if(digitalRead(BUTTON_PIN_CH) == HIGH)
+        {
+            if(setTimeState == 0)
+            {
+                setHour = setHour + 1;
+            }
+            else if(setTimeState == 1)
+            {
+                setMinutes = setMinutes + 1;
+            }
+        }
 
-      displayThrough74HC595(firstDigit(setHour), lastDigit(setHour), firstDigit(setMinutes), lastDigit(setMinutes));
+        displayThrough74HC595(firstDigit(setHour), lastDigit(setHour), firstDigit(setMinutes), lastDigit(setMinutes));
 
-      delay(250);
+        delay(250);
 
-      if(setTimeState == 2)
-      {
-        MCP7940.adjust(DateTime(2018,11,11,setHour,setMinutes,0));   //Set a specific time
-      }
+        if(setTimeState == 2)
+        {
+            MCP7940.adjust(DateTime(2018,11,11,setHour,setMinutes,0));   //Set a specific time
+        }
     }
     else
     {
-          DateTime now = MCP7940.now();
+        DateTime now = MCP7940.now();
 
-    if (secs != now.second())
-    {
-        sprintf(inputBuffer,"%04d-%02d-%02d %02d:%02d:%02d", now.year(),
-            now.month(), now.day(), now.hour(), now.minute(), now.second());
+        if (secs != now.second())
+        {
+            sprintf(inputBuffer,"%04d-%02d-%02d %02d:%02d:%02d", now.year(),
+                    now.month(), now.day(), now.hour(), now.minute(), now.second());
 
-        if(SERIAL_ACTIVE == 1)
+            if(SERIAL_ACTIVE == 1)
             Serial.println(inputBuffer);
 
-        secs = now.second();
+            secs = now.second();
 
-        // Toogle the led when reading time
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
+            // Toogle the led when reading time
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
 
-        displayThrough74HC595(firstDigit(now.hour()), lastDigit(now.hour()), firstDigit(now.minute()), lastDigit(now.minute()));
-
+            displayThrough74HC595(firstDigit(now.hour()), lastDigit(now.hour()), firstDigit(now.minute()), lastDigit(now.minute()));
+        }
     }
-    }
-
-
 
     delay(100);
 }
